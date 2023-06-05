b1a7e21efbae4673159ce75ea5825fdf3f3ccf28
Ayhem Allouache <58494917+AyhemAllouache@users.noreply.github.com>
2023-06-05 18:20:53 +0100

    Create cotrolRobotLiveStreamServerDetectionYolo.py

---
+++ cotrolRobotLiveStreamServerDetectionYolo.py
@@ -0,0 +1,121 @@
+from kafka import KafkaConsumer
+import cv2
+import numpy as np
+from flask import Flask, render_template, request, Response, jsonify
+from kafka import KafkaProducer
+import json
+
+app = Flask(__name__)
+
+
+topic1 = 'test'
+topic = 'carControl'
+topic2 = 'serverDetection'
+consumer = KafkaConsumer(
+    topic1,
+    bootstrap_servers=['192.168.195.217:9092'],
+    auto_offset_reset='latest'
+)
+consumer2 = KafkaConsumer(
+    topic2,
+    bootstrap_servers=['192.168.195.217:9092'],
+    auto_offset_reset='latest'
+)
+producer = KafkaProducer(bootstrap_servers=['192.168.195.217:9092'], value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
+
+#_______________________________ the functions car control __________________________
+def forward():
+    producer.send(topic, 'forward')
+
+def backward():
+    producer.send(topic, 'backward')
+
+def left():
+    producer.send(topic, 'left')
+
+def right():
+    producer.send(topic, 'right')
+
+def stop():
+    producer.send(topic, 'stop')
+    
+def auto():
+    producer.send(topic, 'auto')
+    
+def manual():
+    producer.send(topic, 'manual')
+
+
+#_____________________________________________________________________
+#_______________________________ the functions live stream __________________________
+def gen_frames():
+    for msg in consumer:
+        nparr = np.frombuffer(msg.value, np.uint8)
+        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        # resize the frame to a smaller size
+        frame = cv2.resize(frame, (640, 480))
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])[1].tobytes() + b'\r\n')
+
+#_____________________________________________________________________
+
+#_______________________________ the functions live stream2 __________________________
+def gen_frames_detect():
+    for msg in consumer2:
+        nparr = np.frombuffer(msg.value, np.uint8)
+        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        # resize the frame to a smaller size
+        frame = cv2.resize(frame, (640, 480))
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])[1].tobytes() + b'\r\n')
+
+#_____________________________________________________________________
+
+
+#_______________________________ Routes __________________________
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/", methods=["POST", "GET"])
+def control():
+    if request.method == "POST":
+        data = request.get_json()
+        command = data.get('command')
+
+        if command == 'forward':
+            forward()
+        elif command == 'backward':
+            backward()
+        elif command == 'left':
+            left()
+        elif command == 'right':
+            right()
+        elif command == 'stop':
+            stop()
+        elif command == 'manual':
+            manual()
+        elif command == 'auto':
+            auto()
+
+        return jsonify({'status': 'success'})
+    
+    return render_template("index.html")
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed_video')
+def video_feed_video():
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed_detect')
+def video_feed_detect():
+    return Response(gen_frames_detect(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
